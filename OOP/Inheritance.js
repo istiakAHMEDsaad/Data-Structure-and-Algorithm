13b6009503d882a0b91be58763c94d682c8ca5ab
istiakAHAMMEDsaad <istik2020@gmail.com>
2026-08-25 17:56:29 +0600

    inheritance

---
+++ OOP/Inheritance.js
@@ -0,0 +1,45 @@
+// Isolate Impact of Change
+class Player {
+  #name;
+  #age;
+
+  constructor(name, age) {
+    this.#name = name;
+    this.#age = age;
+  }
+
+  getPlayerDetails() {
+    return `${this.#name} is ${this.#age} years old`;
+  }
+}
+
+class Cricketer extends Player {
+  #centuries;
+
+  constructor(name, age, centuries) {
+    super(name, age);
+    this.#centuries = centuries;
+  }
+
+  getPlayerDetails() {
+    return `${super.getPlayerDetails()} & total centuries is ${this.#centuries}`;
+  }
+}
+
+class Footballer extends Player {
+  #goals;
+
+  constructor(name, age, goals) {
+    super(name, age);
+    this.#goals = goals;
+  }
+
+  getPlayerDetails() {
+    return `${super.getPlayerDetails()} & total goals is ${this.#goals}`;
+  }
+}
+
+const robiul = new Cricketer("Robiul", 32, 1);
+const motin = new Footballer("Motin", 28, 13);
+
+console.log(robiul.getPlayerDetails());
